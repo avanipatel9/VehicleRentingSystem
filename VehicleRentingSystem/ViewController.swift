8a96ae0abe3b260cb7244719bfe0a6bdc49e58c2
Avani Patel <anpatel36@yahoo.in>
2020-03-18 19:51:59 -0400

    Initial Commit

---
+++ VehicleRentingSystem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VehicleRentingSystem
+//
+//  Created by Avani Patel on 3/18/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
