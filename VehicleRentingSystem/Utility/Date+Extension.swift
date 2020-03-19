f44af797a209fcb18a2351d0106dc1a48ec5cd84
Avani Patel <anpatel36@yahoo.in>
2020-03-19 18:46:19 -0400

    [AP] Date extension added

---
+++ VehicleRentingSystem/Utility/Date+Extension.swift
@@ -0,0 +1,29 @@
+//
+//  Date+Extension.swift
+//  VehicleRentingSystem
+//
+//  Created by Avani Patel on 3/19/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import Foundation
+extension Date
+{
+    public func getForamttedDate() -> String
+    {
+        let dateFormatterPrint = DateFormatter()
+        dateFormatterPrint.dateFormat = "EEEE, dd MMMM, yyyy"
+        let formattedDate = dateFormatterPrint.string(from: self)
+        return formattedDate
+    }
+    
+    static func from(year: Int, month: Int, day: Int) -> Date?
+    {
+        let calendar = Calendar(identifier: .gregorian)
+        var dateComponents = DateComponents()
+        dateComponents.year = year
+        dateComponents.month = month
+        dateComponents.day = day
+        return calendar.date(from: dateComponents) ?? nil
+    }
+}
