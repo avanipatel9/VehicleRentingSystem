685c7740efa890e1af133ace1ef496a0b32c623d
Avani Patel <anpatel36@yahoo.in>
2020-03-19 18:47:48 -0400

    [AP] String extension added

---
+++ VehicleRentingSystem/Utility/String+Extension.swift
@@ -0,0 +1,48 @@
+//
+//  String+Extension.swift
+//  VehicleRentingSystem
+//
+//  Created by Avani Patel on 3/19/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import Foundation
+extension String
+{
+    func isValidEmail() -> Bool
+    {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self)
+    }
+    
+    func isValidPhone() -> Bool
+    {
+        let PHONE_REGEX = "^(?:\\+?1[-.●]?)?\\(?([0-9]{3})\\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$"
+        let phoneTest = NSPredicate(format: "SELF MATCHES %@", PHONE_REGEX)
+        return phoneTest.evaluate(with: self)
+    }
+    
+    func encryptPassword() -> String {
+        let characters = Array(self)
+        var encryptString:String=String();
+        for char  in characters
+        {
+            var ascii = char.asciiValue!;
+            ascii+=3;
+            encryptString.append(Character(UnicodeScalar(ascii))) //https://www.dotnetperls.com/convert-int-character-swift
+        }
+        return encryptString;
+    }
+    func decryptPassword() -> String {
+        let characters = Array(self)
+        var decryptString:String=String();
+        for char  in characters
+        {
+            var ascii = char.asciiValue!;
+            ascii-=3;
+            decryptString.append(Character(UnicodeScalar(ascii)))
+        }
+        return decryptString;
+    }
+}
